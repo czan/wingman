2285e504aa2a9ee49f71625fd747f934a66718f7
Carlo Zancanaro <carlo@zancanaro.id.au>
2018-04-04 21:43:56 +1000

    Update url and license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dont-give-up "0.1.0-SNAPSHOT"
   :description "Restarts for Clojure."
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/czan/dont-give-up"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
