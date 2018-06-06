687c7fa691f27ba6e8395b0c64d80336651f0d9b
Carlo Zancanaro <carlo@zancanaro.id.au>
2018-06-06 10:35:41 +1000

    Add some tests for core

--- core/test/dont_give_up/core_test.clj
+++ core/test/dont_give_up/core_test.clj
@@ -1,3 +1,41 @@
 (ns dont-give-up.core-test
   (:require [clojure.test :refer :all]
             [dont-give-up.core :refer :all]))
+
+(deftest make-restart-passes-restart?
+  (is (restart? (make-restart "name" nil nil #(do)))))
+
+(deftest restart?-fails-other-objects
+  (is (not (restart? nil)))
+  (is (not (restart? :key)))
+  (is (not (restart? "str")))
+  (is (not (restart? 10))))
+
+(deftest list-restarts-returns-empty-outside-of-handlers
+  (call-with-restarts (fn [ex]
+                        [(make-restart :r1 nil nil (constantly nil))])
+    #(is (= (map :name (list-restarts))
+            []))))
+
+(deftest list-restarts-returns-current-restarts-inside-handlers
+  (call-with-handler (fn [ex]
+                       (is (= (map :name (list-restarts))
+                              [:r1])))
+    #(call-with-restarts (fn [ex]
+                                [(make-restart :r1 nil nil (constantly nil))])
+       (fn []
+         (/ 1 0)))))
+
+(deftest rethrow-throws-exceptions
+  (is (thrown? Exception
+               (rethrow (Exception.))))
+  (is (thrown? Exception
+               (call-with-handler (fn [ex]
+                                    (rethrow ex))
+                 #(throw (Exception.)))))
+  (is (thrown? Exception
+               (call-with-handler (fn [ex]
+                                    (rethrow ex))
+                 #(call-with-restarts (fn [ex] [])
+                    (fn []
+                      (throw (Exception.))))))))
